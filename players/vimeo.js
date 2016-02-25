c4bc4088be15b41f1b37fd8db714bb1e8e04e52a
alejandrodob <alejandrodob@gmail.com>
2016-02-25 12:22:49 +0100

    Froogalop must be inside the init function, otherwise postMessages fail

--- players/vimeo.js
+++ players/vimeo.js
@@ -1,217 +1,218 @@
-const vimeoApi = (function(){
-    function Froogaloop(iframe) {
-        return new Froogaloop.fn.init(iframe);
-    }
-
-    let eventCallbacks = {},
-        hasWindowEvent = false,
-        isReady = false,
-        slice = Array.prototype.slice,
-        playerOrigin = '*';
-
-    Froogaloop.fn = Froogaloop.prototype = {
-        element: null,
-
-        init: function(iframe) {
-            if (typeof iframe === "string") {
-                iframe = document.getElementById(iframe);
-            }
-
-            this.element = iframe;
-
-            return this;
-        },
-
-        api: function(method, valueOrCallback) {
-            if (!this.element || !method) {
-                return false;
-            }
-
-            let self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null,
-                params = !isFunction(valueOrCallback) ? valueOrCallback : null,
-                callback = isFunction(valueOrCallback) ? valueOrCallback : null;
-
-            if (callback) {
-                storeCallback(method, callback, target_id);
-            }
-
-            postMessage(method, params, element);
-            return self;
-        },
-
-        addEvent: function(eventName, callback) {
-            if (!this.element) {
-                return false;
-            }
-
-            let self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null;
-
-
-            storeCallback(eventName, callback, target_id);
-
-            if (eventName != 'ready') {
-                postMessage('addEventListener', eventName, element);
-            }
-            else if (eventName == 'ready' && isReady) {
-                callback.call(null, target_id);
-            }
-
-            return self;
-        },
-
-        removeEvent: function(eventName) {
-            if (!this.element) {
-                return false;
-            }
-
-            let self = this,
-                element = self.element,
-                target_id = element.id !== '' ? element.id : null,
-                removed = removeCallbacks(eventName, target_id);
-
-            if (eventName != 'ready' && removed) {
-                postMessage('removeEventListener', eventName, element);
-            }
-        }
-    };
-
-    function postMessage(method, params, target) {
-        if (!target.contentWindow.postMessage) {
-            return false;
-        }
-
-        let data = JSON.stringify({
-            method: method,
-            value: params
-        });
-
-        target.contentWindow.postMessage(data, playerOrigin);
-    }
-
-    function onMessageReceived(event) {
-        let data, method;
-
-        try {
-            data = JSON.parse(event.data);
-            method = data.event || data.method;
-        }
-        catch(e) {
-        }
-
-        if (method == 'ready' && !isReady) {
-            isReady = true;
-        }
-
-        if (!(/^https?:\/\/player.vimeo.com/).test(event.origin)) {
-            return false;
-        }
-
-        if (playerOrigin === '*') {
-            playerOrigin = event.origin;
-        }
-
-        let value = data.value,
-            eventData = data.data,
-            target_id = target_id === '' ? null : data.player_id,
-
-            callbacks = getCallbacks(method, target_id),
-            params = [];
-
-        if (!callbacks) {
-            return false;
-        }
-
-        if (value !== undefined) {
-            params.push(value);
-        }
-
-        if (eventData) {
-            params.push(eventData);
-        }
-
-        if (target_id) {
-            params.push(target_id);
-        }
-
-        callbacks.forEach(function(callback) {
-            params.length > 0 ? callback.apply(null, params) : callback.call();
-        });
-    }
-
-    function storeCallback(eventName, callback, target_id) {
-        if (target_id) {
-            if (!eventCallbacks[target_id]) {
-                eventCallbacks[target_id] = {};
-            }
-            if (!eventCallbacks[target_id][eventName]) {
-                eventCallbacks[target_id][eventName] = [];
-            }
-            eventCallbacks[target_id][eventName].push(callback);
-        }
-        else {
-            if (!eventCallbacks[eventName]) {
-                eventCallbacks[eventName] = [];
-            }
-            eventCallbacks[eventName].push(callback);
-        }
-    }
-
-    function getCallbacks(eventName, target_id) {
-        if (target_id) {
-            return eventCallbacks[target_id][eventName];
-        }
-        else {
-            return eventCallbacks[eventName];
-        }
-    }
-
-    function removeCallbacks(eventName, target_id) {
-        if (target_id && eventCallbacks[target_id]) {
-            if (!eventCallbacks[target_id][eventName]) {
-                return false;
-            }
-            eventCallbacks[target_id][eventName] = null;
-        }
-        else {
-            if (!eventCallbacks[eventName]) {
-                return false;
-            }
-            eventCallbacks[eventName] = null;
-        }
-
-        return true;
-    }
-
-    function isFunction(obj) {
-        return !!(obj && obj.constructor && obj.call && obj.apply);
-    }
-
-    function isArray(obj) {
-        return toString.call(obj) === '[object Array]';
-    }
-
-    Froogaloop.fn.init.prototype = Froogaloop.fn;
-
-    if (window.addEventListener) {
-        window.addEventListener('message', onMessageReceived, false);
-    }
-    else {
-        window.attachEvent('onmessage', onMessageReceived);
-    }
-
-    return Froogaloop;
-
-})();
-
 const VimeoPlayer = function() {
     let player,
         playerContainer;
 
     function init(videoUrl, containerId) {
+        const vimeoApi = (function(){
+            function Froogaloop(iframe) {
+                return new Froogaloop.fn.init(iframe);
+            }
+
+            let eventCallbacks = {},
+                hasWindowEvent = false,
+                isReady = false,
+                slice = Array.prototype.slice,
+                playerOrigin = '*';
+
+            Froogaloop.fn = Froogaloop.prototype = {
+                element: null,
+
+                init: function(iframe) {
+                    if (typeof iframe === "string") {
+                        iframe = document.getElementById(iframe);
+                    }
+
+                    this.element = iframe;
+
+                    return this;
+                },
+
+                api: function(method, valueOrCallback) {
+                    if (!this.element || !method) {
+                        return false;
+                    }
+
+                    let self = this,
+                        element = self.element,
+                        target_id = element.id !== '' ? element.id : null,
+                        params = !isFunction(valueOrCallback) ? valueOrCallback : null,
+                        callback = isFunction(valueOrCallback) ? valueOrCallback : null;
+
+                    if (callback) {
+                        storeCallback(method, callback, target_id);
+                    }
+
+                    postMessage(method, params, element);
+                    return self;
+                },
+
+                addEvent: function(eventName, callback) {
+                    if (!this.element) {
+                        return false;
+                    }
+
+                    let self = this,
+                        element = self.element,
+                        target_id = element.id !== '' ? element.id : null;
+
+
+                    storeCallback(eventName, callback, target_id);
+
+                    if (eventName != 'ready') {
+                        postMessage('addEventListener', eventName, element);
+                    }
+                    else if (eventName == 'ready' && isReady) {
+                        callback.call(null, target_id);
+                    }
+
+                    return self;
+                },
+
+                removeEvent: function(eventName) {
+                    if (!this.element) {
+                        return false;
+                    }
+
+                    let self = this,
+                        element = self.element,
+                        target_id = element.id !== '' ? element.id : null,
+                        removed = removeCallbacks(eventName, target_id);
+
+                    if (eventName != 'ready' && removed) {
+                        postMessage('removeEventListener', eventName, element);
+                    }
+                }
+            };
+
+            function postMessage(method, params, target) {
+                if (!target.contentWindow.postMessage) {
+                    return false;
+                }
+
+                let data = JSON.stringify({
+                    method: method,
+                    value: params
+                });
+
+                target.contentWindow.postMessage(data, playerOrigin);
+            }
+
+            function onMessageReceived(event) {
+                let data, method;
+
+                try {
+                    data = JSON.parse(event.data);
+                    method = data.event || data.method;
+                }
+                catch(e) {
+                }
+
+                if (method == 'ready' && !isReady) {
+                    isReady = true;
+                }
+
+                if (!(/^https?:\/\/player.vimeo.com/).test(event.origin)) {
+                    return false;
+                }
+
+                if (playerOrigin === '*') {
+                    playerOrigin = event.origin;
+                }
+
+                let value = data.value,
+                    eventData = data.data,
+                    target_id = target_id === '' ? null : data.player_id,
+
+                    callbacks = getCallbacks(method, target_id),
+                    params = [];
+
+                if (!callbacks) {
+                    return false;
+                }
+
+                if (value !== undefined) {
+                    params.push(value);
+                }
+
+                if (eventData) {
+                    params.push(eventData);
+                }
+
+                if (target_id) {
+                    params.push(target_id);
+                }
+
+                callbacks.forEach(function(callback) {
+                    params.length > 0 ? callback.apply(null, params) : callback.call();
+                });
+            }
+
+            function storeCallback(eventName, callback, target_id) {
+                if (target_id) {
+                    if (!eventCallbacks[target_id]) {
+                        eventCallbacks[target_id] = {};
+                    }
+                    if (!eventCallbacks[target_id][eventName]) {
+                        eventCallbacks[target_id][eventName] = [];
+                    }
+                    eventCallbacks[target_id][eventName].push(callback);
+                }
+                else {
+                    if (!eventCallbacks[eventName]) {
+                        eventCallbacks[eventName] = [];
+                    }
+                    eventCallbacks[eventName].push(callback);
+                }
+            }
+
+            function getCallbacks(eventName, target_id) {
+                if (target_id) {
+                    return eventCallbacks[target_id][eventName];
+                }
+                else {
+                    return eventCallbacks[eventName];
+                }
+            }
+
+            function removeCallbacks(eventName, target_id) {
+                if (target_id && eventCallbacks[target_id]) {
+                    if (!eventCallbacks[target_id][eventName]) {
+                        return false;
+                    }
+                    eventCallbacks[target_id][eventName] = null;
+                }
+                else {
+                    if (!eventCallbacks[eventName]) {
+                        return false;
+                    }
+                    eventCallbacks[eventName] = null;
+                }
+
+                return true;
+            }
+
+            function isFunction(obj) {
+                return !!(obj && obj.constructor && obj.call && obj.apply);
+            }
+
+            function isArray(obj) {
+                return toString.call(obj) === '[object Array]';
+            }
+
+            Froogaloop.fn.init.prototype = Froogaloop.fn;
+
+            if (window.addEventListener) {
+                window.addEventListener('message', onMessageReceived, false);
+            }
+            else {
+                window.attachEvent('onmessage', onMessageReceived);
+            }
+
+            return Froogaloop;
+
+        })();
+
+
         this.baseUrl = 'https://player.vimeo.com/video/';
         this.videoId = videoUrl.split('//player.vimeo.com/video/')[1];
 
