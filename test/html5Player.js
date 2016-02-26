ac8b1ee07c08d368054f8b934b9353588d72db76
alejandrodob <alejandrodob@gmail.com>
2016-02-26 11:48:23 +0100

    Test play/pause/destroy for html5 player

---
+++ test/html5Player.js
@@ -0,0 +1,65 @@
+import test from 'tape';
+import PybossaPlayer from '../player';
+
+const containerId = 'player-container';
+const html5audioUrl = 'https://raw.githubusercontent.com/PyBossa/pybossa-player.js/master/test/files/audio.mp3';
+const html5videoUrl = 'https://raw.githubusercontent.com/PyBossa/pybossa-player.js/master/test/files/video.mp4';
+
+function setUp() {
+  const body = document.body;
+  body.innerHTML = '';
+  const container = document.createElement('div');
+  container.setAttribute('id', containerId);
+  body.appendChild(container);
+}
+
+
+test('HTML5 player accepts a callback that is fired when ready', (assert) => {
+  setUp();
+  assert.plan(1);
+  const player = PybossaPlayer(html5videoUrl, containerId);
+
+  player.onReady(function() {
+    assert.pass("Player is ready");
+    assert.end();
+  });
+});
+
+test('HTML5 player can be played when ready', (assert) => {
+  setUp();
+  assert.plan(1);
+  const player = PybossaPlayer(html5videoUrl, containerId);
+
+  player.onReady(function() {
+    player.play();
+    assert.equals(player.paused(), false);
+    assert.end();
+  });
+});
+
+test('HTML5 player can be paused when is playing', (assert) => {
+  setUp();
+  assert.plan(2);
+  const player = PybossaPlayer(html5videoUrl, containerId);
+
+  player.onReady(function() {
+    player.play();
+    assert.equals(player.paused(), false);
+    player.pause();
+    assert.equals(player.paused(), true);
+    assert.end();
+  });
+});
+
+test('HTML5 player.destroy() removes the player from DOM', (assert) => {
+  setUp();
+  assert.plan(2);
+  const player = PybossaPlayer(html5videoUrl, containerId);
+
+  assert.ok(document.getElementsByTagName('video')[0]);
+
+  player.destroy();
+
+  assert.notOk(document.getElementsByTagName('video')[0]);
+});
+
