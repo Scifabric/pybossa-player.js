5aacb7b62f9328d7fdc587dbde66cfcdd113c607
alejandrodob <alejandrodob@gmail.com>
2016-02-28 20:44:36 +0100

    Test youtube urls

--- test/youtube.js
+++ test/youtube.js
@@ -1,5 +1,6 @@
 import test from 'tape';
 import PybossaPlayer from '../player';
+import YoutubePlayer from '../players/youtube';
 
 const containerId = 'player-container';
 const youtubeUrl = 'https://www.youtube.com/watch?v=M4TH8rU8wZw';
@@ -207,4 +208,39 @@ test('Youtube player.onPlayTimeChange() accepts a callback fired when playtime c
     });
     player.play();
   });
+});
+
+test('Youtube player recognises Youtube URLs', (assert) => {
+  let urls = [
+    '//www.youtube-nocookie.com/embed/up_lNV-yoK4?rel=0',
+    'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo',
+    'http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel',
+    'http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub',
+    'http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I',
+    'http://www.youtube.com/user/SilkRoadTheatre#p/a/u/2/6dwqZw0j_jY',
+    'http://youtu.be/6dwqZw0j_jY',
+    'http://www.youtube.com/watch?v=6dwqZw0j_jY&feature=youtu.be',
+    'http://youtu.be/afa-5HQHiAs',
+    'http://www.youtube.com/user/Scobleizer#p/u/1/1p3vcRhsYGo?rel=0',
+    'http://www.youtube.com/watch?v=cKZDdG9FTKY&feature=channel',
+    'http://www.youtube.com/watch?v=yZ-K7nCVnBI&playnext_from=TL&videos=osPknwzXEas&feature=sub',
+    'http://www.youtube.com/ytscreeningroom?v=NRHVzbJVx8I',
+    'http://www.youtube.com/embed/nas1rJpm7wY?rel=0',
+    'http://www.youtube.com/watch?v=peFZbP64dsU',
+    'http://youtube.com/v/dQw4w9WgXcQ?feature=youtube_gdata_player',
+    'http://youtube.com/vi/dQw4w9WgXcQ?feature=youtube_gdata_player',
+    'http://youtube.com/?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+    'http://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+    'http://youtube.com/?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
+    'http://youtube.com/watch?v=dQw4w9WgXcQ&feature=youtube_gdata_player',
+    'http://youtube.com/watch?vi=dQw4w9WgXcQ&feature=youtube_gdata_player',
+    'http://youtu.be/dQw4w9WgXcQ?feature=youtube_gdata_player'
+  ];
+
+  let youtubePlayer = YoutubePlayer();
+
+  for (let i = 0; i < urls.length; ++i) {
+      assert.ok(youtubePlayer.isYoutubeLink(urls[i]));
+  }
+  assert.end();
 });
